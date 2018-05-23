d4abb289a4b5ce266c9684c2a926795f1041cdff
David Naranjo <david.naranjogarcia@outlook.es>
2018-05-23 15:49:14 +0100

    lesson GetYourHandsDirty 1st part finished

--- 05_GetYourHandsDirty/practice.php
+++ 05_GetYourHandsDirty/practice.php
@@ -8,23 +8,19 @@
   $fav_color = "Black"
   $birth_year = 1995;
 
-	/*
-	Use PHP to calculate the difference
-	between your birth year and this year
-	to show your age dynamically
-	*/
   date_default_timezone_set('Europe/London');
 
   $today = date('F j, Y');
   $this_year = date('Y');
 
+  $myage = ($this_year - $birth_year);
 
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Get Your Hands Dirty: <!-- PAGE TITLE --></title>
+		<title>Get Your Hands Dirty: <?php echo TITLE; ?></title>
 		<link href="../assets/styles.css" rel="stylesheet">
 	</head>
 	<body>
@@ -33,23 +29,23 @@
 				<img src="../assets/img/logo.png" alt="PHP">
 			</a>
 
-			<h1>Get Your Hands Dirty: <small><!-- PAGE TITLE --></small></h1>
+			<h1>Get Your Hands Dirty: <small><?php echo TITLE; ?></small></h1>
 			<hr>
 
 			<h2>Your Example</h2>
 
 			<div class="sandbox">
 				<h3>Today's Date:</h3>
-				<p><!-- DATE --></p>
+				<p><?php echo $today; ?></p>
 
 				<h3>My Name:</h3>
-				<p><!-- YOUR NAME --></p>
+				<p><?php echo $my_name; ?></p>
 
 				<h3>My Favourite Colour:</h3>
-				<p><!-- FAVOURITE COLOUR --></p>
+				<p><?php echo $fav_color; ?></p>
 
 				<h3>My Age:</h3>
-				<p><!-- YOUR AGE --></p>
+				<p><?php echo $myage; ?></p>
 			</div><!-- end sandbox -->
 
 			<a href="index.php" class="button">Back to the final example</a>
